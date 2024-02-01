0d5101cbb9c35002e2b759aba9f17d20cd696240
Anil Shrestha <>
2024-02-01 23:11:19 +0545

    v1

---
+++ IELauncher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IELauncher
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        static void Main(string[] args)
+        {
+            SHDocVw.InternetExplorer IE = new SHDocVw.InternetExplorer();
+            object URL;
+            ShowWindow((IntPtr)IE.HWND, 3);
+            if (args.Length >= 1)
+            {
+                URL = args[0];
+                IE.Navigate2(ref URL);
+            }
+            else
+            {
+                //URL = "about:blank"; // home page
+                IE.GoHome();
+            }
+
+            IE.Visible = true;           
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# IE Launcher
+
+## IE Launcher for Windows 10 and 11
+
+### Introduction
+
+If you are still using IE and your windows installation does not Support IE any more, IELauncher is the tool for you.
